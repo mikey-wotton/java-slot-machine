bf60a077e0df95094a3ba6f5150c5dfe9212c5cb
mw509 <mw509@kent.ac.uk>
2016-09-21 23:07:48 +0100

    added modularity to wheel generation each wheel generation has its own method now, allows creating different wheels with different numbers of symbols

--- SuperSecretProject/src/RandomiseWheels.java
+++ SuperSecretProject/src/RandomiseWheels.java
@@ -25,9 +25,8 @@ public class RandomiseWheels {
 	 * which are then displayed to the user and checked for wins against other wheels
 	 * 
 	 */
-	public static int[] pickFiveWinners(){
+	public static int[] pickFiveWinners(int[] array){
 	    Random rnd = new Random();
-	    int[] array = generateWheel();
 		int index = rnd.nextInt(array.length+1);
 		int[] pickedFive = new int[5];
 		for(int i = 0; i < pickedFive.length; i++ ){
@@ -46,10 +45,120 @@ public class RandomiseWheels {
 	 * 40 Tens, 30 Jacks, 20 Queens, 7 Kings, 3 Aces
 	 * it then shuffles those variables with shuffleArray
 	 * @return	shuffle array of strings
-	 */
-	
-	
-	public static int[] generateWheel(){
+	 */		
+	public static int[] generateFirstWheel(){
+		int[] wheel = new int[100];
+		for(int i = 0; i < wheel.length; i++){
+			if (isBetween(i, 0, 39)) {
+					wheel[i] = 0;
+				}
+			else if (isBetween(i, 40, 69 )) {
+					wheel[i] = 1;
+				}
+			else if (isBetween(i, 70, 89)) {
+					wheel[i] = 2;
+					}
+			else if (isBetween(i, 90, 96)) {
+					wheel[i] = 3;
+					}
+			else if (isBetween(i, 97, 99)) {
+					wheel[i] = 4;
+					}				
+		}	
+		shuffleArray(wheel);
+		return wheel;
+	}
+	/**
+	 * This method should generate a single wheel with 100 variables
+	 * 40 Tens, 30 Jacks, 20 Queens, 7 Kings, 3 Aces
+	 * it then shuffles those variables with shuffleArray
+	 * @return	shuffle array of strings
+	 */	
+	public static int[] generateSecondWheel(){
+		int[] wheel = new int[100];
+		for(int i = 0; i < wheel.length; i++){
+			if (isBetween(i, 0, 39)) {
+					wheel[i] = 0;
+				}
+			else if (isBetween(i, 40, 69 )) {
+					wheel[i] = 1;
+				}
+			else if (isBetween(i, 70, 89)) {
+					wheel[i] = 2;
+					}
+			else if (isBetween(i, 90, 96)) {
+					wheel[i] = 3;
+					}
+			else if (isBetween(i, 97, 99)) {
+					wheel[i] = 4;
+					}				
+		}	
+		shuffleArray(wheel);
+		return wheel;
+	}
+	/**
+	 * This method should generate a single wheel with 100 variables
+	 * 40 Tens, 30 Jacks, 20 Queens, 7 Kings, 3 Aces
+	 * it then shuffles those variables with shuffleArray
+	 * @return	shuffle array of strings
+	 */	
+	public static int[] generateThirdWheel(){
+		int[] wheel = new int[100];
+		for(int i = 0; i < wheel.length; i++){
+			if (isBetween(i, 0, 39)) {
+					wheel[i] = 0;
+				}
+			else if (isBetween(i, 40, 69 )) {
+					wheel[i] = 1;
+				}
+			else if (isBetween(i, 70, 89)) {
+					wheel[i] = 2;
+					}
+			else if (isBetween(i, 90, 96)) {
+					wheel[i] = 3;
+					}
+			else if (isBetween(i, 97, 99)) {
+					wheel[i] = 4;
+					}				
+		}	
+		shuffleArray(wheel);
+		return wheel;
+	}
+	/**
+	 * This method should generate a single wheel with 100 variables
+	 * 40 Tens, 30 Jacks, 20 Queens, 7 Kings, 3 Aces
+	 * it then shuffles those variables with shuffleArray
+	 * @return	shuffle array of strings
+	 */	
+	public static int[] generateFourthWheel(){
+		int[] wheel = new int[100];
+		for(int i = 0; i < wheel.length; i++){
+			if (isBetween(i, 0, 39)) {
+					wheel[i] = 0;
+				}
+			else if (isBetween(i, 40, 69 )) {
+					wheel[i] = 1;
+				}
+			else if (isBetween(i, 70, 89)) {
+					wheel[i] = 2;
+					}
+			else if (isBetween(i, 90, 96)) {
+					wheel[i] = 3;
+					}
+			else if (isBetween(i, 97, 99)) {
+					wheel[i] = 4;
+					}				
+		}	
+		shuffleArray(wheel);
+		return wheel;
+	}
+	/**
+	 * This method should generate a single wheel with 100 variables
+	 * 40 Tens, 30 Jacks, 20 Queens, 7 Kings, 3 Aces
+	 * it then shuffles those variables with shuffleArray
+	 * @return	shuffle array of strings
+	 */	
+	public static int[] generateFifthWheel(){
 		int[] wheel = new int[100];
 		for(int i = 0; i < wheel.length; i++){
 			if (isBetween(i, 0, 39)) {
