9370698cb2e0c08b944c90c1eab8d36375e337ce
mw509 <mw509@kent.ac.uk>
2016-10-21 07:33:03 +0100

    added label generation loop

--- SuperSecretProject/src/GUI.java
+++ SuperSecretProject/src/GUI.java
@@ -1,9 +1,11 @@
 import java.awt.EventQueue;
 import java.awt.Dimension;
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JSpinner;
+import javax.swing.OverlayLayout;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingWorker;
 
@@ -13,9 +15,9 @@ import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
 
 import java.awt.Color;
+import java.util.Random;
 
 import javax.swing.SwingConstants;
-
 import javax.swing.ImageIcon;
 import javax.swing.UIManager;
 
@@ -44,7 +46,7 @@ public class GUI {
 	 * Create the application.
 	 */
 	public GUI() {
-		main = new Main("John", 500.00);
+		main = new Main("Mr. Lister", 500.00);
 		initialize();
 	}
 
@@ -128,7 +130,7 @@ public class GUI {
 
 		JLabel lblUserName = new JLabel("Username: "
 				+ main.userDetails.getUsername());
-		lblUserName.setBounds(10, 10, 100, 20);
+		lblUserName.setBounds(10, 10, 200, 20);
 		contentPane.add(lblUserName);
 
 		JLabel lblBalance = new JLabel("Balance: "
@@ -156,162 +158,25 @@ public class GUI {
 		winStakeSpinner.setBounds(828, 31, 50, 20);
 		contentPane.add(winStakeSpinner);
 
-		// All labels for the images of the wheels
-		JLabel winLine = new JLabel("winLine");
-		winLine.setIcon(new ImageIcon("line_1.png"));
-		winLine.setBounds(245,45,10,377);
-		contentPane.add(winLine);
+		//JPanel overLayPanel = new JPanel(new OverlayLayout())
 		
-		JLabel label = new JLabel("0 - 0");
-		label.setIcon(new ImageIcon("facedown_small.jpg"));
-		label.setBounds(215, 10, 70, 90);
-		contentPane.add(label);
-		labels[0][0] = label;
-
-		JLabel label_0 = new JLabel("0 - 1");
-		label_0.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_0.setBounds(215, 101, 70, 90);
-		contentPane.add(label_0);
-		labels[0][1] = label_0;
-
-		JLabel label_1 = new JLabel("0 - 2");
-		label_1.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_1.setBounds(215, 192, 70, 90);
-		contentPane.add(label_1);
-		labels[0][2] = label_1;
-
-		JLabel label_2 = new JLabel("0 - 3");
-		label_2.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_2.setBounds(215, 283, 70, 90);
-		contentPane.add(label_2);
-		labels[0][3] = label_2;
-
-		JLabel label_3 = new JLabel("0 - 4");
-		label_3.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_3.setBounds(215, 374, 70, 90);
-		contentPane.add(label_3);
-		labels[0][4] = label_3;
-
-		JLabel label_5 = new JLabel("1 - 0");
-		label_5.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_5.setBounds(310, 10, 70, 90);
-		contentPane.add(label_5);
-		labels[1][0] = label_5;
-
-		JLabel label_6 = new JLabel("1 - 1");
-		label_6.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_6.setBounds(310, 101, 70, 90);
-		contentPane.add(label_6);
-		labels[1][1] = label_6;
-
-		JLabel label_7 = new JLabel("1 - 2");
-		label_7.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_7.setBounds(310, 192, 70, 90);
-		contentPane.add(label_7);
-		labels[1][2] = label_7;
-
-		JLabel label_8 = new JLabel("1 - 3");
-		label_8.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_8.setBounds(310, 283, 70, 90);
-		contentPane.add(label_8);
-		labels[1][3] = label_8;
-
-		JLabel label_9 = new JLabel("1 - 4");
-		label_9.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_9.setBounds(310, 374, 70, 90);
-		contentPane.add(label_9);
-		labels[1][4] = label_9;
-
-		JLabel label_10 = new JLabel("2 - 0");
-		label_10.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_10.setBounds(404, 10, 70, 90);
-		contentPane.add(label_10);
-		labels[2][0] = label_10;
-
-		JLabel label_11 = new JLabel("2 - 1");
-		label_11.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_11.setBounds(404, 101, 70, 90);
-		contentPane.add(label_11);
-		labels[2][1] = label_11;
-
-		JLabel label_12 = new JLabel("2 - 2");
-		label_12.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_12.setBounds(404, 192, 70, 90);
-		contentPane.add(label_12);
-		labels[2][2] = label_12;
-
-		JLabel label_13 = new JLabel("2 - 3");
-		label_13.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_13.setBounds(404, 283, 70, 90);
-		contentPane.add(label_13);
-		labels[2][3] = label_13;
-
-		JLabel label_14 = new JLabel("2 -4");
-		label_14.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_14.setBounds(404, 374, 70, 90);
-		contentPane.add(label_14);
-		labels[2][4] = label_14;
-
-		JLabel label_15 = new JLabel("3 - 0");
-		label_15.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_15.setBounds(498, 10, 70, 90);
-		contentPane.add(label_15);
-		labels[3][0] = label_15;
-
-		JLabel label_16 = new JLabel("3 - 1");
-		label_16.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_16.setBounds(498, 101, 70, 90);
-		contentPane.add(label_16);
-		labels[3][1] = label_16;
-
-		JLabel label_17 = new JLabel("3 - 2");
-		label_17.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_17.setBounds(498, 192, 70, 90);
-		contentPane.add(label_17);
-		labels[3][2] = label_17;
-
-		JLabel label_18 = new JLabel("3 - 3");
-		label_18.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_18.setBounds(498, 283, 70, 90);
-		contentPane.add(label_18);
-		labels[3][3] = label_18;
-
-		JLabel label_19 = new JLabel("3 - 4");
-		label_19.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_19.setBounds(498, 374, 70, 90);
-		contentPane.add(label_19);
-		labels[3][4] = label_19;
-
-		JLabel label_20 = new JLabel("4 - 0");
-		label_20.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_20.setBounds(592, 10, 70, 90);
-		contentPane.add(label_20);
-		labels[4][0] = label_20;
-
-		JLabel label_21 = new JLabel("4 - 1");
-		label_21.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_21.setBounds(592, 101, 70, 90);
-		contentPane.add(label_21);
-		labels[4][1] = label_21;
-
-		JLabel label_22 = new JLabel("4 - 2");
-		label_22.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_22.setBounds(592, 192, 70, 90);
-		contentPane.add(label_22);
-		labels[4][2] = label_22;
-
-		JLabel label_23 = new JLabel("4 - 3");
-		label_23.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_23.setBounds(592, 283, 70, 90);
-		contentPane.add(label_23);
-		labels[4][3] = label_23;
-
-		JLabel label_24 = new JLabel("4 - 4");
-		label_24.setIcon(new ImageIcon("facedown_small.jpg"));
-		label_24.setBackground(new Color(0, 0, 150, 30));
-		label_24.setBounds(592, 374, 70, 90);
-		contentPane.add(label_24);
-		labels[4][4] = label_24;
+		JPanel cardPanel = new JPanel();
+		cardPanel.setLayout(null);
+		cardPanel.setBounds(215, 10, 450, 460);	
+		cardPanel.setBackground(new Color(250, 78, 91));
+		
+		// All labels for the images of the wheels
+		for(int i = 0; i < 5; i++){
+			int setX = i * 95;			
+			for(int j =0; j < 5; j++){ 
+				int setY = j * 92;
+				labels[i][j] = new JLabel(i+ " "+j);
+				labels[i][j].setIcon(new ImageIcon("facedown_small.jpg"));
+				labels[i][j].setBounds(setX,setY,70,90);
+				labels[i][j].setOpaque(false);
+				cardPanel.add(labels[i][j]);
+			}
+		}
 
 		JLabel lblWinOrLoseAmount = new JLabel("winOrLoseAmount");
 		lblWinOrLoseAmount.setText("Welcome, good luck!");
@@ -354,6 +219,7 @@ public class GUI {
 			}
 		});
 		contentPane.add(autoSpin);
+		contentPane.add(cardPanel);
 		return contentPane;
 	}
 
@@ -388,14 +254,14 @@ public class GUI {
 						labels[i][j]
 								.setIcon(new ImageIcon("facedown_small.jpg"));
 						try {
-							Thread.sleep(50); // 1000 milliseconds is one
+							Thread.sleep(20); // 1000 milliseconds is one
 												// second.
 						} catch (InterruptedException ex) {
 							Thread.currentThread().interrupt();
 						}
 					}
 				}
-				
+
 				main.spinOnce(numOfWinLines, lineStake);
 				for (int i = 0; i < main.arrayOfWheels.length; i++) {
 					for (int j = 0; j < main.arrayOfWheels.length; j++) {
